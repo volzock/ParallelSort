bf9b37ec766fcb9b5bcf08d0493964b6467ce5a6
volzock <nukfeed@yandex.ru>
2025-03-08 13:18:42 +0300

    add non parallel MergeSort

---
+++ src/main/java/com/github/volzock/MergeSort.java
@@ -0,0 +1,73 @@
+package com.github.volzock;
+
+import lombok.NonNull;
+
+public class MergeSort {
+    private MergeSort(){}
+
+    public static void sort(@NonNull int[] arr) {
+        final var left = new SortTaskInformation(0, arr.length);
+        final var right = new SortTaskInformation(arr.length, 0);
+
+        sort(arr, left, right);
+    }
+
+    public static void sort(@NonNull int[] arr,
+                            @NonNull final SortTaskInformation left,
+                            @NonNull final SortTaskInformation right) {
+        if (left.length > 1) {
+            final var leftHalfLength = left.length() / 2;
+            final var leftLeft = new SortTaskInformation(left.startPosition, leftHalfLength);
+            final var leftRight = new SortTaskInformation(left.startPosition + leftHalfLength,
+                    left.length - leftHalfLength);
+            sort(arr, leftLeft, leftRight);
+        }
+
+        if (right.length > 1) {
+            final var rightHalfLength = right.length / 2;
+            final var rightLeft = new SortTaskInformation(right.startPosition, rightHalfLength);
+            final var rightRight = new SortTaskInformation(right.startPosition + rightHalfLength,
+                    right.length - rightHalfLength);
+            sort(arr, rightLeft, rightRight);
+        }
+
+        merge(arr, left, right);
+    }
+
+    public static void merge(@NonNull int[] arr,
+                             @NonNull final SortTaskInformation left,
+                             @NonNull final SortTaskInformation right) {
+        int[] leftArr = new int[left.length];
+        int[] rightArr = new int[right.length];
+
+        if (left.length >= 1) {
+            System.arraycopy(arr, left.startPosition, leftArr, 0, left.length);
+        }
+        if (right.length >= 1) {
+            System.arraycopy(arr, right.startPosition, rightArr, 0, right.length);
+        }
+
+        int leftIndex = 0, rightIndex = 0;
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (leftArr[leftIndex] < rightArr[rightIndex]) {
+                arr[left.startPosition + leftIndex + rightIndex] = leftArr[leftIndex];
+                ++leftIndex;
+            } else {
+                arr[left.startPosition + leftIndex + rightIndex] = rightArr[rightIndex];
+                ++rightIndex;
+            }
+        }
+
+        while (leftIndex < left.length) {
+            arr[left.startPosition + leftIndex + rightIndex] = leftArr[leftIndex];
+            ++leftIndex;
+        }
+
+        while (rightIndex < right.length) {
+            arr[left.startPosition + leftIndex + rightIndex] = rightArr[rightIndex];
+            ++rightIndex;
+        }
+    }
+
+    public record SortTaskInformation(int startPosition, int length){}
+}
--- src/test/java/com/github/volzock/AppTest.java
+++ src/test/java/com/github/volzock/AppTest.java
@@ -1,19 +1,105 @@
 package com.github.volzock;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void emptyArray() {
+        int[] arr = {};
+        int[] expected = {};
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void oneSizeArray() {
+        int[] arr = {1};
+        int[] expected = {1};
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void SizeOfTwoArray() {
+        int[] arr = {2, 1};
+        int[] expected = {1, 2};
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void SizeOfThreeArray() {
+        int[] arr = {3, 2, 1};
+        int[] expected = {1, 2, 3};
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+
+        arr = new int[]{2, 1, 3};
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void SizeOfFiveArray() {
+        int[] arr = {3, 5, 2, 1, 4};
+        int[] expected = {1, 2, 3, 4, 5};
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+        MergeSort.sort(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void SizeOfOneHundredArray() {
+        int[] arr = randomArray(100);
+        int[] expected = new int[100];
+        System.arraycopy(arr, 0, expected, 0, arr.length);
+
+        MergeSort.sort(arr);
+        Arrays.sort(expected);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void SizeOfOneThousandArray() {
+        int[] arr = randomArray(1000);
+        int[] expected = new int[1000];
+        System.arraycopy(arr, 0, expected, 0, arr.length);
+
+        MergeSort.sort(arr);
+        Arrays.sort(expected);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void SizeOfTenHundredArray() {
+        int[] arr = randomArray(10000);
+        int[] expected = new int[10000];
+        System.arraycopy(arr, 0, expected, 0, arr.length);
+
+        MergeSort.sort(arr);
+        Arrays.sort(expected);
+        assertArrayEquals(expected, arr);
+    }
+
+    private int[] randomArray(int size) {
+        final var random = new Random();
+        random.setSeed(0);
+
+        return IntStream.generate(() -> random.nextInt(100)).limit(size).toArray();
     }
 }
